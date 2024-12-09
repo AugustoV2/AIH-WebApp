e1e0ed7289c556426cceae347c92ba7d66098dc4
krthik777 <karthikv9778@gmail.com>
2024-12-09 22:40:20 +0530

    stylesssss

--- components/map.tsx
+++ components/map.tsx
@@ -2,24 +2,20 @@
 
 import React from "react";
 
-//src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao&q=Amal+Jyothi+College+of+Engineering+Autonomous,+9.52866,76.82344&zoom=18`)
-
-
 const GoogleMap = () => {
   return (
     <div className="mb-5 flex justify-center">
-    <div className="h-[30rem] w-[1200px] border-2 border-gray-300 rounded-lg shadow-lg">
-          <iframe
-            title="Google Map"
-            width="100%"
-            height="100%"
-            style={{ border: 0, borderRadius: "15px" }}
-            loading="lazy"
-            allowFullScreen
-            src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao
-                &q=9.52866,76.82344&zoom=18`}
-    //      src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao&q=Amal+Jyothi+College+of+Engineering+Autonomous,+9.52866,76.82344&zoom=18`}
-          ></iframe>
+      <div className="mx-3 h-[20rem] w-full sm:h-[25rem] sm:w-[600px] lg:h-[30rem] lg:w-[1200px] border-2 border-gray-300 rounded-lg shadow-lg">
+        <iframe
+          title="Google Map"
+          width="100%"
+          height="100%"
+          style={{ border: 0, borderRadius: "15px" }}
+          loading="lazy"
+          allowFullScreen
+          src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao
+                &q=Amal+Jyothi+College+of+Engineering+Autonomous,+9.52866,76.82344&zoom=18`}
+        ></iframe>
       </div>
     </div>
   );
