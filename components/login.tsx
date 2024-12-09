ad4a58dfa3344675b167b5c05fd06270aa43a28a
AugustoV2 <augustovarghese@gmail.com>
2024-12-09 16:40:47 +0530

    edits

--- components/login.tsx
+++ components/login.tsx
@@ -78,6 +78,7 @@ export default function App() {
               labelPlacement="outside"
               name="email"
               placeholder="Enter your email"
+              
               type="email"
             />
 
