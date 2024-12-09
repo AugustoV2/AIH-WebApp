7c5997508ea1c6ace1ca3a90e3311dbd3a46bce2
krthik777 <karthikv9778@gmail.com>
2024-12-09 22:31:21 +0530

    ..

--- components/map.tsx
+++ components/map.tsx
@@ -2,10 +2,13 @@
 
 import React from "react";
 
+//src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao&q=Amal+Jyothi+College+of+Engineering+Autonomous,+9.52866,76.82344&zoom=18`)
+
+
 const GoogleMap = () => {
   return (
     <div className="mb-5 flex justify-center">
-      <div className="h-96 w-[900px] border-2 border-gray-300 rounded-lg shadow-lg">
+    <div className="h-[30rem] w-[1200px] border-2 border-gray-300 rounded-lg shadow-lg">
           <iframe
             title="Google Map"
             width="100%"
@@ -14,7 +17,8 @@ const GoogleMap = () => {
             loading="lazy"
             allowFullScreen
             src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao
-                &q=9.52866,76.82344&zoom=16`}
+                &q=9.52866,76.82344&zoom=18`}
+    //      src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAOVYRIgupAurZup5y1PRh8Ismb1A3lLao&q=Amal+Jyothi+College+of+Engineering+Autonomous,+9.52866,76.82344&zoom=18`}
           ></iframe>
       </div>
     </div>
