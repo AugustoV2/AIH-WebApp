d8599a3993fd6c6be50781b48a7a755eed5cee9f
AugustoV2 <augustovarghese@gmail.com>
2024-12-10 21:46:20 +0530

    aaa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = {
+const nextConfig = {
   images: {
     domains: ["envs.sh"], // Allow images from this domain
   },
 };
+
+module.exports = nextConfig;
